176364e0ee407a958733f952e43d94c9eb61b56c
Vijay-Hozo <vijay2304a@gmail.com>
2024-08-09 14:33:01 +0530

    added

--- Server/Controllers/CartController.js
+++ Server/Controllers/CartController.js
@@ -1,22 +1,23 @@
 const CartModel = require("../Models/CartModel");
 const ProductModel = require("../Models/ProductModel");
 const mongoose = require("mongoose");
-const ObjectId = mongoose.Types.ObjectId;
 
 // ADD-TO-CART
 const addtocart = async (req, res) => {
-  const { productid, quantity } = req.body;
-  const userid = req.user.id;
+  const { product_id, quantity } = req.body;
+  const user_id = req.user.id;
+  console.log(user_id);
+  
 
-  if (!productid || !userid || quantity == null) {
+  if (!product_id || !user_id || quantity == null) {
     return res.status(401).send({ message: "Cannot add item, missing data." });
   }
 
   try {
-    const cart = await CartModel.findOne({ userid });
+    const cart = await CartModel.findOne({ user_id });
 
     if (cart) {
-      const oldProduct = cart.products.find((p) => p.product_id === productid);
+      const oldProduct = cart.products.find((p) => p.product_id === product_id);
       if (oldProduct) {
         oldProduct.quantity += quantity;
         await cart.save();
@@ -39,18 +40,19 @@ const addtocart = async (req, res) => {
 
 // GET-ITEMS
 const getcarts = async (req, res) => {
-  const userid = req.user.id;
+  const user_id = req.user.id;
 
   try {
-    const cart = await CartModel.findOne({ userid });
+    const cart = await CartModel.findOne({ user_id });
 
     if (cart) {
       let subtotal = 0;
       const productDetails = await Promise.all(
         cart.products.map(async (item) => {
-          const product = await ProductModel.findOne({ id: item.productid });
+          const product = await ProductModel.findOne({ id: item.product_id });
           subtotal += product.price * item.quantity;
           return {
+            id:product.id,
             title: product.title,
             description: product.description,
             image: product.image,
